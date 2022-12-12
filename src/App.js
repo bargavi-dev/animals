99044c60ebb932f43a6f3fb330f5e94acdbae423
Bargavi  Kanneganti <bargavi.tech@gmail.com>
2022-12-12 11:15:24 -0600

    more of section 4 complete

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
+import { useState } from 'react';
+
+
 function App() {
-//   const handleClick = () => {
-//     console.log("Button was clicked!");
-//   };
+  function makeArray() {
+    return [1, 10, 32, 40];
+  }
+
+  // const [firstElement, secondElement] =makeArray();
+  // console.log(firstElement, secondElement);
+
+  const [count, setCount] = useState(0);
+
+  console.log( useState(50));
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
 
   return (
     <div>
         <button 
-            onClick={() => 
-                console.log('Button was clicked!')}>
-            Add Animal
-        </button>
+            onClick={handleClick}>Add Animal</button>
+            <div>Number of animals: {count}</div>
     </div>
   );
 }
